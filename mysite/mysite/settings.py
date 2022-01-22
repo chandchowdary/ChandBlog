428a2fed34e85e396ac57a84d5740bf7673041d4
chand <saichandeluri@gmail.com>
2022-01-22 15:34:32 -0500

    allowed host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m3xd#renop62v@#rtp1cuqef5#ei6rnch!0h^xy#w1u=$ssmw!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saichand-site.eba-pfuqknqh.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
